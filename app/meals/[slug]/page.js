0ac9f8cc8039abe09d18dd35db12b05078996e5b
mryunt02 <mrthresh@gmail.com>
2024-04-28 14:49:19 +0300

    Dynamic routing and fetching data

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,32 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
 function MealDetailsPage({ params }) {
+  const meal = getMeal(params.slug);
+
   return (
-    <div>
-      <h1>{params.slug}</h1>
-    </div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br>"),
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
 export default MealDetailsPage;
